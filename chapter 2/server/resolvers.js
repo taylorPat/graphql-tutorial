3b2dced3f2b782a6b8a3ac0b5d339ad4ac932ae8
pat taylor <patrick.schneider9@web.de>
2024-01-21 14:05:25 +0100

    feat(schema): get multiple jobs

--- chapter 2/server/resolvers.js
+++ chapter 2/server/resolvers.js
@@ -1,10 +1,18 @@
 export const resolvers = {
     Query: {
-        job: () => {
-            return {
+        jobs: () => {
+            return [
+                {
+                id: '123456789',
                 title: 'The Title',
                 description: 'Description',
-            }
+            },
+            {
+                id: '3456789',
+                title: 'ABC',
+                description: 'DEF',
+            },
+        ]
         },
     },
 };
