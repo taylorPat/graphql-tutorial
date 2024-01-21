4444e8c5fa19e67b8d9ec288bedf6e4b2edd4d67
pat taylor <patrick.schneider9@web.de>
2024-01-21 17:05:00 +0100

    feat: add company object type and add object association between job and company

--- chapter 2/server/resolvers.js
+++ chapter 2/server/resolvers.js
@@ -1,7 +1,18 @@
+import { getCompany } from "./db/companies.js";
 import { getJobs } from "./db/jobs.js";
 
 export const resolvers = {
     Query: {
-        jobs: async () => getJobs(),
+        jobs: () => getJobs(),
     },
+    Job: {
+        date: (job) => mapDate(job.createdAt),
+        company: (job) => getCompany(job.companyId)
+    },
+
+    
 };
+
+function mapDate(date) {
+    return date.slice(0, 'yyyy-mm-dd'.length)
+}
