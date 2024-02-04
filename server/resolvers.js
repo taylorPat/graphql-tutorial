4781be671733545c6a5fd0202ebdf71f19dcc571
pat taylor <patrick.schneider9@web.de>
2024-02-04 20:24:48 +0100

    feat: add server code for create, delete and update and client code for create

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,6 +1,6 @@
 import { GraphQLError } from 'graphql';
 import { getCompany } from './db/companies.js';
-import { getJob, getJobs, getJobsByCompany } from './db/jobs.js';
+import { createJob, deleteJob, getJob, getJobs, getJobsByCompany, updateJob } from './db/jobs.js';
 
 export const resolvers = {
   Query: {
@@ -19,6 +19,23 @@ export const resolvers = {
       return job;
     },
     jobs: () => getJobs(),
+  },
+
+  Mutation: {
+    createJob: (_root, { input: {title, description }}) => {
+      console.log(`Root: ${_root}, `)
+      const companyId = 'FjcJCHJALA4i'
+      const job = createJob({companyId, title, description})
+      return job
+    },
+    deleteJob: async (_root, { id }) => {
+      const job = await deleteJob(id)
+      return job
+    },
+    updateJob: async (_root, {input: { id, title, description }}) => {
+      const job = await updateJob({ id, title, description})
+      return job
+    }
   },
 
   Company: {
