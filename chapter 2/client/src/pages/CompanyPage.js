391b6f338ff9cbc386ede28b99ec69cc5dccaa72
pat taylor <patrick.schneider9@web.de>
2024-02-04 11:43:10 +0100

    feat: query jobs

--- chapter 2/client/src/pages/CompanyPage.js
+++ chapter 2/client/src/pages/CompanyPage.js
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router';
 import { useEffect, useState } from 'react';
 import { getCompany } from '../lib/graphql/queries.js'
+import JobList from '../components/JobList';
 
 function CompanyPage() {
   const { companyId } = useParams();
@@ -23,6 +24,7 @@ function CompanyPage() {
       <div className="box">
         {company.description}
       </div>
+      <JobList jobs={company.jobs} />
     </div>
   );
 }
--- chapter 2/server/resolvers.js
+++ chapter 2/server/resolvers.js
@@ -1,11 +1,14 @@
 import { getCompany } from "./db/companies.js";
-import { getJobs, getJob } from "./db/jobs.js";
+import { getJobs, getJob, getJobsByCompany } from "./db/jobs.js";
 
 export const resolvers = {
     Query: {
         job: (_root, {id}) => getJob(id),
         company: (_root, {id}) => getCompany(id),
         jobs: () => getJobs(),
+    },
+    Company : {
+        jobs: (company) => getJobsByCompany(company.id),
     },
     Job: {
         date: (job) => mapDate(job.createdAt),
